8f0819f9b07adad814dea96cbab730ccc7c1fcc0
4C4F4943 <74591660+4C4F4943@users.noreply.github.com>
2020-11-21 10:41:01 +0100

    uwu

--- text_in_box.h
+++ text_in_box.h
@@ -1,20 +1,27 @@
 #include<iostream>
+#include "color_output.h"
 using namespace std;
-string text_in_box(string the_thing){
+string text_in_box(string the_thing,string color1, string color2){
 
-	int the_length = the_thing.size();
-	for(int j = 0; j < the_length+ 21; j++){if(j == the_length + 20){cout<<"|"<<endl;}else if(j == 0){cout<<"|";}else{cout<<"-";}}
-        for(int j = 0; j < the_length+ 21; j++){if(j == the_length + 20){cout<<"|"<<endl;}else if(j == 0){cout<<"|";}else{cout<<"-";}}
-        for(int i = 0; i < 22; i ++)
-          {       
-                  if(i == 10){cout<<the_thing;}
-                  else if(i == 0){cout<<"|";}
-                  else if(i == 21){cout<<"|";}
-                  else{cout<<"-";} 
-         }
-          
-          cout<<endl; 
-          for(int j = 0; j < the_length+ 21; j++){if(j == the_length + 20){cout<<"|"<<endl;}else if(j == 0){cout<<"|";}else{cout<<"_";}}
-          for(int j = 0; j < the_length+ 21; j++){if(j == the_length + 20){cout<<"|"<<endl;}else if(j == 0){cout<<"|";}else{cout<<"_";}}
-	return 0;
-}
+        int the_length = the_thing.size();
+
+ for(int j = 0; j < the_length+ 21; j++){if(j == the_length + 20){text_background("|",color1);cout<<endl;}else if(j == 0){text_background("|",color1);}else{text_background("-",color1);}}
+    for(int j = 0; j < the_length+ 21; j++){if(j == the_length + 20){text_background("|",color1);cout<<endl;}else if(j == 0){text_background("|",color1);}else{text_background("-",color1);}}
+    for(int i = 0; i < 22; i ++)
+
+    {
+        if(i == 10)
+        {
+        text_background(the_thing, color2);
+        }
+        else if(i == 0){text_background("|",color1);}
+        else if(i == 21){text_background("|",color1);}
+        else
+        {
+        text_background("-",color1);
+        }
+    }
+    cout<<endl;
+    for(int j = 0; j < the_length+ 21; j++){if(j == the_length + 20){text_background("|",color1);cout<<endl;}else if(j == 0){text_background("|",color1);}else{text_background("_",color1);}}
+    for(int j = 0; j < the_length+ 21; j++){if(j == the_length + 20){text_background("|",color1);cout<<endl;}else if(j == 0){text_background("|",color1);}else{text_background("_",color1);}}
+return 0;
